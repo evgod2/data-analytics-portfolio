5bcef1ad78f630b1c2371383d15e6b2e5d382342
evgod2 <evgod2@gmail.com>
2026-06-21 17:15:14 -0600

    Update summary_reports.sql

--- projects/sql-projects/financial-pipeline-optimization/queries/summary_reports.sql
+++ projects/sql-projects/financial-pipeline-optimization/queries/summary_reports.sql
@@ -6,3 +6,12 @@ SELECT
 FROM `evan10725.financial_transactions`
 GROUP BY date
 ORDER BY date;
+
+SELECT
+    account_id,
+    COUNT(*) AS transaction_count,
+    SUM(amount) AS total_spent,
+    AVG(amount) AS avg_amount
+FROM `evan10725.financial_transactions`
+GROUP BY account_id
+ORDER BY total_spent DESC;
