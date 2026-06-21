39d842a16f478f8ede5b10d48bc31371024d78f0
evgod2 <evgod2@gmail.com>
2026-06-21 17:07:55 -0600

    Create main_analysis.sql

---
+++ projects/sql-projects/financial-pipeline-optimization/queries/main_analysis.sql
@@ -0,0 +1,24 @@
+WITH ordered AS (
+    SELECT
+        account_id,
+        transaction_timestamp,
+        amount,
+        LAG(amount) OVER (
+            PARTITION BY account_id
+            ORDER BY transaction_timestamp
+        ) AS prev_amount
+    FROM `evan10725.financial_transactions`
+)
+
+SELECT
+    account_id,
+    transaction_timestamp,
+    amount,
+    prev_amount,
+    CASE
+        WHEN prev_amount IS NOT NULL AND amount > prev_amount * 3
+        THEN 'velocity_spike'
+        ELSE 'normal'
+    END AS anomaly_flag
+FROM ordered
+ORDER BY account_id, transaction_timestamp;
